264787d0ec54122e176abb7d280efab050d9a7c4
OPApple <80098382+OPApple@users.noreply.github.com>
2022-08-28 22:10:43 +0300

    Made a thing

---
+++ src/Components/Square.js
@@ -0,0 +1,147 @@
+import React, { Component } from 'react'
+
+
+
+
+export default class Square extends Component {
+  render() {
+    return (
+      <div className='Square'>
+
+        
+          <input maxLength={1} className='top-left'/>
+          <input maxLength={1} className='top-left'/>
+          <input maxLength={1} className='top-left'/>
+
+          <input maxLength={1} className='top-mid'/>
+          <input maxLength={1} className='top-mid'/>
+          <input maxLength={1} className='top-mid'/>
+
+          <input maxLength={1} className='top-right'/>
+          <input maxLength={1} className='top-right'/>
+          <input maxLength={1} className='top-right'/>
+
+
+          <input maxLength={1} className='top-left'/>
+          <input maxLength={1} className='top-left'/>
+          <input maxLength={1} className='top-left'/>
+
+
+          <input maxLength={1} className='top-mid'/>
+          <input maxLength={1} className='top-mid'/>
+          <input maxLength={1} className='top-mid'/>
+
+
+          <input maxLength={1} className='top-right'/>
+          <input maxLength={1} className='top-right'/>
+          <input maxLength={1} className='top-right'/>
+
+
+          <input maxLength={1} className='top-left'/>
+          <input maxLength={1} className='top-left'/>
+          <input maxLength={1} className='top-left'/>
+
+
+          <input maxLength={1} className='top-mid'/>
+          <input maxLength={1} className='top-mid'/>
+          <input maxLength={1} className='top-mid'/>
+          
+          
+          <input maxLength={1} className='top-right'/>
+          <input maxLength={1} className='top-right'/>
+          <input maxLength={1} className='top-right'/>
+          
+          
+          <input maxLength={1} className='mid-left'/>
+          <input maxLength={1} className='mid-left'/>
+          <input maxLength={1} className='mid-left'/>
+
+
+          <input maxLength={1} className='mid-mid'/>
+          <input maxLength={1} className='mid-mid'/>
+          <input maxLength={1} className='mid-mid'/>
+          
+          
+          <input maxLength={1} className='mid-right'/>
+          <input maxLength={1} className='mid-right'/>
+          <input maxLength={1} className='mid-right'/>
+          
+          
+          <input maxLength={1} className='mid-left'/>
+          <input maxLength={1} className='mid-left'/>
+          <input maxLength={1} className='mid-left'/>
+
+
+          <input maxLength={1} className='mid-mid'/>
+          <input maxLength={1} className='mid-mid'/>
+          <input maxLength={1} className='mid-mid'/>
+          
+          
+          <input maxLength={1} className='mid-right'/>
+          <input maxLength={1} className='mid-right'/>
+          <input maxLength={1} className='mid-right'/>
+          
+          
+          <input maxLength={1} className='mid-left'/>
+          <input maxLength={1} className='mid-left'/>
+          <input maxLength={1} className='mid-left'/>
+
+
+          <input maxLength={1} className='mid-mid'/>
+          <input maxLength={1} className='mid-mid'/>
+          <input maxLength={1} className='mid-mid'/>
+          
+          
+          <input maxLength={1} className='mid-right'/>
+          <input maxLength={1} className='mid-right'/>
+          <input maxLength={1} className='mid-right'/>
+          
+          
+          <input maxLength={1} className='bottom-left'/>
+          <input maxLength={1} className='bottom-left'/>
+          <input maxLength={1} className='bottom-left'/>
+
+
+          <input maxLength={1} className='bottom-mid'/>
+          <input maxLength={1} className='bottom-mid'/>
+          <input maxLength={1} className='bottom-mid'/>
+          
+          
+          <input maxLength={1} className='bottom-right'/>
+          <input maxLength={1} className='bottom-right'/>
+          <input maxLength={1} className='bottom-right'/>
+          
+          
+          <input maxLength={1} className='bottom-left'/>
+          <input maxLength={1} className='bottom-left'/>
+          <input maxLength={1} className='bottom-left'/>
+
+
+          <input maxLength={1} className='bottom-mid'/>
+          <input maxLength={1} className='bottom-mid'/>
+          <input maxLength={1} className='bottom-mid'/>
+          
+          
+          <input maxLength={1} className='bottom-right'/>
+          <input maxLength={1} className='bottom-right'/>
+          <input maxLength={1} className='bottom-right'/>
+          
+          
+          <input maxLength={1} className='bottom-left'/>
+          <input maxLength={1} className='bottom-left'/>
+          <input maxLength={1} className='bottom-left'/>
+
+
+          <input maxLength={1} className='bottom-mid'/>
+          <input maxLength={1} className='bottom-mid'/>
+          <input maxLength={1} className='bottom-mid'/>
+          
+          
+          <input maxLength={1} className='bottom-right'/>
+          <input maxLength={1} className='bottom-right'/>
+          <input maxLength={1} className='bottom-right'/>
+
+      </div>
+    )
+  }
+}
---
+++ src/Components/Three.js
@@ -0,0 +1,9 @@
+import React, { PureComponent } from 'react'
+
+export default class Three extends PureComponent {
+  render() {
+    return (
+      <div>Three</div>
+    )
+  }
+}
